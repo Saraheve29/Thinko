af9d01e9cde88d95e1b3dccf5b74f6c38ba60fba
Saraheve29 <sarahwhyte33@gmail.com>
2026-05-14 14:25:03 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../Thinko.jsx'
+import App from '../thinko_v25.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
